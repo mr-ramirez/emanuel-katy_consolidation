ea561472469ae791e51780fcbc9076d07d5e2c8e
Jacob Ramirez <jacob@acklenavenue.com>
2018-05-26 21:59:31 -0500

    Setting the google api key from env variable

--- config/test.env.js
+++ config/test.env.js
@@ -3,5 +3,6 @@ const merge = require('webpack-merge')
 const devEnv = require('./dev.env')
 
 module.exports = merge(devEnv, {
-  NODE_ENV: '"testing"'
+  NODE_ENV: '"testing"',
+  GOOGLE_API_KEY: `"${process.env.GOOGLE_API_KEY}"`,
 })
